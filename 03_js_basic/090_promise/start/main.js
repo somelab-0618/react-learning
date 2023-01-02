d06600941f7bdcf72fc1945f00a6f0bc0f387d6f
RyosukeSomeya <rx.0.0618@gmail.com>
2023-01-03 00:28:42 +0900

    promiseのメモ追加2

--- 03_js_basic/090_promise/start/main.js
+++ 03_js_basic/090_promise/start/main.js
@@ -20,3 +20,21 @@ new Promise((resolve, reject) => {
     // rejectが実行された時に実行される
     console.log('catchが実行', c);
   });
+
+// awaitとasync
+init();
+async function init() {
+  try {
+    const res = await new Promise((resolve, reject) => {
+      setTimeout(() => {
+        a = 1;
+        // 処理が正常に終了した場合
+        resolve(a); // 引数に値を渡すことでthenのコールバック関数に引数として渡っていく
+      }, 2000);
+    });
+    console.log(res);
+  } catch (e) {
+    // 何らかエラーかrejectが実行された時に実行される
+    console.log('catchが実行', e);
+  }
+}
