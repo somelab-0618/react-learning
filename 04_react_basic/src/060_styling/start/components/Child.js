a66be2172cf2d79fde1a3ea9844ef002a9b197bf
RyosukeSomeya <rx.0.0618@gmail.com>
2023-01-04 00:11:34 +0900

    コンポーネント分割まで

--- 04_react_basic/src/060_styling/start/components/Child.js
+++ 04_react_basic/src/060_styling/start/components/Child.js
@@ -1,9 +1,16 @@
 /**
  * [注意]レクチャーをプルダウンで選択すると、<head>タグにそのレクチャーでimport "Example.css"のように読み込んだスタイルが挿入されます。このスタイルはプルダウンを切り替えても残りつづけるため、まだcssを記述していないのにスタイルが適用されていると感じた場合にはブラウザを更新してください。
  */
-import Child from './components/Child';
-import './Example.css';
+import { List } from './List';
+import './Child.css';
 
-const Example = () => <Child />;
+const Example = () => {
+  return (
+    <div className={'component'}>
+      <h3>Hello Component</h3>
+      <List />
+    </div>
+  );
+};
 
 export default Example;
